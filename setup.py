27162377a4db97082f9855b5b05765f064405440
mayway500 <manunanayawsarpongstreetorrich@gmail.com>
2025-10-22 21:12:53 +0800

    Create setup.py for AnFuelpriceEnv package
    Added setup.py for package configuration and dependencies.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AnFuelpriceEnv",
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torch_geometric',
+        'pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv',
+        'networkx'
+        'torchrl',
+        'tensordict',
+        'pandas',
+        'numpy',
+        'pyyaml',
+        'ipdb',
+        'gymnasium',
+        'pyyaml',
+        'wandb',
+        'tensorboard',
+        
+        
+        # ... add any other dependencies here
+    ],
+    # ... other setup options if needed
+)
+
+
